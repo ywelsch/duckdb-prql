1b4d4b149058208cc835b7cad66172697d1675a3
Yannick Welsch <yannick@welsch.lu>
2025-03-17 12:02:01 +0100

    towards wasm support

--- extension_config.cmake
+++ extension_config.cmake
@@ -3,5 +3,6 @@
 # Extension from this repo
 duckdb_extension_load(prql
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+    LINKED_LIBS "$(CMAKE_BINARY_DIR)/third_party/re2/libduckdb_re2.a" "$(CMAKE_BINARY_DIR)/extension/prql/libprqlc_c.a"
     LOAD_TESTS
 )
